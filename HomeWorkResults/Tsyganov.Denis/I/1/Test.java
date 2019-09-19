67c6710c5634d3b325a5e4028a5cbde838aed735
Denis Tsyganov <dtsyganov@luxoft.com>
2019-09-19 09:25:40 +0600

    minor changes

---
+++ HomeWorkResults/Tsyganov.Denis/I/1/Test.java
@@ -0,0 +1,10 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Test {
+
+        public static void main(String[] args) {
+            Random random = new Random();
+            Scanner scanner = new Scanner(System.in);
+        }
+    }
